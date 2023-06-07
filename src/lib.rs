7f4d3d5c33b382a6accb9af377f161997ee23ccb
msakuta <masahiro.sakuta@gmail.com>
2023-06-08 00:56:00 +0900

    Add library source

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+extern crate crossbeam;
+extern crate image;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate clap;
+extern crate serde_yaml;
+
+mod hyper_adapt;
+mod modutil;
+mod pixelutil;
+mod quat;
+pub mod render;
+pub mod vec3;
+mod webserver;
